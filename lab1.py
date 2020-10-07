0b4156789bdf1869fb13faf02ae21adc6a5741fd
maciek97x <maciek97x@o2.pl>
2020-10-07 14:29:14 +0200

    init

---
+++ lab1.py
@@ -0,0 +1,32 @@
+import re
+
+def open_file(filename):
+    """
+    Opens file and returns its content.
+
+    Parameters:
+        filename - file name as string
+    
+    Returns:
+        file conctent as string
+
+    Raises:
+        TypeError when filename is not a string
+    """
+    if type(filename) != str:
+        raise TypeError
+
+    return open(filename, 'r').read()
+
+def remove_diacritics(plaintext):
+    if type(plaintext) != str:
+        raise TypeError
+
+    sub_arr = [['ą', 'a'], ['ć', 'c'], ['ę', 'e'], ['ł', 'l'], ['ń', 'n'], ['ó', 'o'], ['ś', 's'], ['ż', 'z'], ['ź', 'z']]
+    for p, s in sub_arr:
+        plaintext = re.sub(p, s, plaintext)
+    return plaintext
+
+def leave_letters_only(plaintext):
+    return re.sub(r'[^a-zA-Z]+', '', plaintext).lower()
+
